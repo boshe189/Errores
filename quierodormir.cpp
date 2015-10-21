21f5a7f18017dc64cbda3f07e0a29ccdda6b3019
boshe189 <mbr189-ca@hotmail.com>
2015-10-21 13:56:06 -0500

    Update quierodormir.cpp

--- quierodormir.cpp
+++ quierodormir.cpp
@@ -4,33 +4,36 @@
 #include <cmath>
 using namespace std;
 
-int sumSquares_lists (vector<int> oper ){
-  int x=0;
-  int sumpot=0;
-  int pot=0;
-  int z=0;
-  z= pow(x,2); {
-return z;
-}
+int sumSquares_lists (){
 
-
-return 0;
-}
-
-
-int main()
-{
-  int x=0;
 
   vector <int> nums;
   cout<<"This program will take your numbers, raise them to squared power and sum those numbers. Introduce the numbers you want to work with. (Write two times zero numerically -> (00) when you want to stop.): "<<endl;
   cin>>x;
   while( x != 00){
   nums.push_back(x);
+int x, i;
   cin >> x;
   }
 
-  cout << "The sum of the power squared of your numbers is: "<< sumSquares_lists(nums) << endl;
+int ope=0;
+for(i=0; i<nums.size(); i++) 
+ope=ope + pow(nums[i],2);
+
+
+
+  cout << "The sum of the power squared of your numbers is: "<< ope << endl;
+
+
+ 
+
+
+int main()
+{
+
+sumSquares_lists();
+  
+
 
 return 0;
 }
